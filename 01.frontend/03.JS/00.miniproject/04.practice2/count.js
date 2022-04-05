f4be6fe3d00301615dced1b6c228b491bfbe6af9
MIHEE PARK <mihee78952@naver.com>
2022-04-05 16:32:38 +0900

    04.05

--- 01.frontend/03.JS/00.miniproject/04.practice2/count.js
+++ 01.frontend/03.JS/00.miniproject/04.practice2/count.js
@@ -1,3 +1,4 @@
+//  
 const [decreaseButton, resetButton, increaseButton] = document.getElementsByClassName('btn');
 //decrease 버튼 눌렀을 때
 // console.log(decreaseButton);
@@ -53,7 +54,14 @@ function commonHandler(number) {
     else countResult.style.color = 'red';
 }
 
-// decrease 버튼 눌렀을 때
+// 콜백 함수에 파라미터 적용시킬려면, 
+// commonHandler(parameter)=> X why? JS파일을 읽으면서 click을 안 했을 때도 commonHandler()함수가 실행됨.
+// 따라서 함수 실행을 잠시 미룰 수 있는 화살표 함수를 사용!!
 decreaseButton.addEventListener('click', () => commonHandler(-1));
 resetButton.addEventListener('click', () => commonHandler(0));
 increaseButton.addEventListener('click',() => commonHandler(1));
+
+// countResult 태그를 객체화 시킬 때 handler()함수 내에서 굳이 반복해서 선언할 필요 없이, 값이 실시간으로 바뀌어도 맨 위에 먼저 객체화를 시켜도 상관없다.
+// 초기에는 버튼마다 callback_function을 선언하여 버튼을 누를 때마다 각각 호출하도록 했다가 반복되는 코드가 많아서, 콜백함수를 하나로 만들면 좋겠다고 생각해서 파라미터 값을 받는 콜백함수로 만들게 됨.
+// callback_funtion(paramer)선언 할 때: () => callback_function(parameter) 형식으로 만들어야한다는 것을 알게 됨.
+// 원래는 count를 판단하는 함수가 따로 있었으나, 콜백함수를 하나로 합치게 되면서 반복사용 되지 않는 이유로 판단함수를 없애고 if문을 그냥 적음.
