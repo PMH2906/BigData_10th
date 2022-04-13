c886245963c1ca084e1310f157f58819325dc275
MIHEE PARK <mihee78952@naver.com>
2022-04-13 10:06:43 +0900

    04.13

--- 01.frontend/MiniProject/server.js
+++ 01.frontend/MiniProject/server.js
@@ -12,9 +12,7 @@ dotenv.config();
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
-
 const request = require('request'); // request 패키지 import
-
 
 // express의 static 미들웨어 활용.
 // express에게 static(정적인 파일) 관련 자원은 public 폴더 내에 있음을 명시.
@@ -30,6 +28,7 @@ app.get('/', (request, response) => {
 });
 
 // 브라우저로부터 감지할 언어의 텍스트를 받아서, Naver 서버로 전송(요청) 및 결과 응답 
+// req: 요청 정보, req.body : 요청 정보 body
 app.post('/detectLangs',(req, res)=>{
     // 127.0.0.1:3000/detectLangs로 요청했을 때
     console.log(typeof req.body, req.body);
@@ -41,7 +40,8 @@ app.post('/detectLangs',(req, res)=>{
 
     const options = {
         url, // 언어감지 네이버 서버의 주소
-        form: { query }, // form이라는 property에 우리가 보낼 데이터를 객체의 형태로 전송
+        // form이라는 property에 우리가 보낼 데이터를 객체의 형태로 전송
+        form: { query }, 
         headers: {
             "X-Naver-Client-Id": clientId,
             "X-Naver-Client-Secret": clientSecret,
@@ -49,7 +49,7 @@ app.post('/detectLangs',(req, res)=>{
     };
     // request.post('요청에 필요한 데이터 동봉', '요청에 따른 응답 정보를 확인하는 콜백함수');
     // options에 요청에 필요한 데이터들을 보관
-    // () => {}: 요청에 따른 응답 정보 확인
+    // () => {}: 요청에 따른 응답 정보 확인 reseponse:응답 정보, body: 응답 정도의 body
     // 언어감지 요청 부분
     request.post(options, (error, response, body)=>{
         if(!error && response.statusCode === 200){
@@ -102,7 +102,7 @@ app.get('/translate',(req,res)=>{
             // 오류
             // res.json(body); // json데이터 형식으로 변환해서 보냄. stringfy()가 적용된 메서드 
 
-            // frontend 에 보내는 코드
+            // frontend(브라우저)에 응답 메시지 전달
             // json 데이터를 보낸다는 것을 표시
             res.setHeader('Content-Type','application/json');
             res.send(body);
