266efb6dfbea9b578cac0447f8b9382081922474
MIHEE PARK <mihee78952@naver.com>
2022-05-19 18:08:51 +0900

    05.19

--- 02.backend/02.web/step04/src/com/mouse/controller/Controller.java
+++ 02.backend/02.web/step04/src/com/mouse/controller/Controller.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 public interface Controller {
 	
 	//execute 메서드 선언
-	// Controller 인터페이스를 구현하는 클래스들은 해당 메서드 
+	// Controller 인터페이스를 구현하는 클래스들은 해당 메서드임.
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 
 }
